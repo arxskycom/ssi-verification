1f84b79e177d5ed0368976176b2df6658187b0c6
Mike Tery <miketery@gmail.com>
2023-04-03 17:09:23 -0400

    added readme and updated main.py

---
+++ README.md
@@ -0,0 +1,56 @@
+# Verification Prototype
+
+This repo is meant to demonstrate how digital trusted relationship can be established.
+The driving principle is public key cryptography, specifically signatures.
+
+## Example Flow
+
+1. NY Times creates a key pair (public and private).
+1. It publishes the public key on its website, e.g. nytimes.com/publickey.txt.
+1. A journalist that works for the Times creates a key pair (public and private).
+1. The journalist creates an object with their public key, and any other information they wish to share (e.g. twitter handle)
+1. The journalist signs this object with their private key.
+1. The journalist sends this object to the Times.
+1. The Times signs this object with their private key.
+
+The final signed object can be used to verify that the Times trusts the journalist.
+
+## Using the Repo
+
+### Environment Setup
+
+1. Install python virtuelenv of choice (only first time):
+
+```
+pip install virtualenv
+virtuelenv -p python3 venv
+source venv/bin/activate && pip install -r requirements.txt
+```
+
+2. Initialize the virtual environment (everytime you visit this project):
+```
+source venv/bin/activate
+```
+
+### Running the Code
+
+```
+python main.py --help
+
+# generate keypairs for NY Times and Alice
+python main.py generate_keypair -n "NY Times" -o nytimes.priv.json
+python main.py generate_keypair -n "Alice Roberts" -o alice.priv.json
+
+# Create object with Alice's public key and twitter handle
+# Note: if you don't have `jq` installed, you can just copy the public key from the file
+echo '{"name": "Alice Roberts", "twitter": "@alice", "public_key": '`jq .public_key alice.priv.json`'}' > alice.to.sign.json
+
+# Sign the object with Alice's private key
+python main.py sign -k alice.priv.json -ts alice.to.sign.json -o alice.signed.json
+
+# Sign the object with NY Times' private key
+python main.py sign -k nytimes.priv.json -ts alice.signed.json -o alice.signed.final.json
+
+# Verify signature?
+python main.py verify -f alice.signed.final.json
+```
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import datetime
 import nacl.encoding
 import nacl.signing
 
-
 to_base58 = lambda data: base58.b58encode(data).decode('utf-8')
 from_base58 = lambda data: base58.b58decode(data.encode('utf-8'))
 
@@ -19,19 +18,20 @@ def load_object_from_file(filename):
     with open(filename, 'r') as f:
         return json.loads(f.read())
 
-def generate_keypair(output_path, name):
+def generate_keypair(output, name):
     # Generate a new signing keypair
     signing_key = nacl.signing.SigningKey.generate()
     verify_key = signing_key.verify_key
 
     # Serialize the keypair to JSON
     serialized_keypair = {
-        "name": name,
         "generated_date": datetime.datetime.now().isoformat(),
         "private_key": to_base58(signing_key.encode()),
         "public_key": to_base58(verify_key.encode()),
     }
-    save_object_to_file(output_path, serialized_keypair)
+    if name:
+        serialized_keypair['name'] = name
+    save_object_to_file(output, serialized_keypair)
     return serialized_keypair
 
 
@@ -53,9 +53,7 @@ def sign_object(key_path, object_to_sign_path, output_path):
 
     # Sign the payload and serialize the result to JSON
     payload_encoded = json.dumps(payload, sort_keys=True).encode()
-    signed_payload = signing_key.sign(
-        payload_encoded,
-        encoder=nacl.encoding.Base64Encoder)
+    signed_payload = signing_key.sign(payload_encoded)
     serialized_signed_payload = {
         "payload": payload,
         "signature": to_base58(signed_payload.signature),
@@ -65,31 +63,67 @@ def sign_object(key_path, object_to_sign_path, output_path):
     return serialized_signed_payload
 
 
+def verify_object(path):
+    serialized_signed_payload_json = load_object_from_file(path)
+    # Extract the public key from the signed payload
+    public_key_bytes = from_base58(serialized_signed_payload_json["public_key"])
+    verify_key = nacl.signing.VerifyKey(public_key_bytes)
+
+    # Serialize the payload and verify the signature
+    payload_encoded = json.dumps(serialized_signed_payload_json["payload"], sort_keys=True).encode()
+    signature_bytes = from_base58(serialized_signed_payload_json["signature"])
+    try:
+        verify_key.verify(payload_encoded, signature_bytes)
+        return True
+    except nacl.exceptions.BadSignatureError:
+        return False
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(dest="command")
+    parser = argparse.ArgumentParser(prog='main.py',
+        description='Script for generating ed25519 keypair, ' +
+        'signing objects, and verifying signed objects. ' +
+        'All bytes are encoded as base58.')
+    sp = parser.add_subparsers(dest="command")
 
-    generate_parser = subparsers.add_parser("generate_keypair")
-    generate_parser.add_argument("output_path", help="Path to save the keypair")
-    generate_parser.add_argument("name", help="The name of the keypair")
+    generate_parser = sp.add_parser("generate_keypair",
+                                    help="Generate a keypair (ed25519).")
+    generate_parser.add_argument("-n", "--name", type=str,
+                                 help="The name of the keypair")
+    generate_parser.add_argument("-o", "--output", type=str,
+                                 help="Path to save the keypair",
+                                 required=True)
 
-    # prepare_parser = subparsers.add_parser("prepare_public_cert")
-    # prepare_parser.add_argument("output", help="The JSON serialized with no private key")
+    sign_parser = sp.add_parser("sign",
+                                help="Sign an object with a private key.")
+    sign_parser.add_argument("-k", "--key-path", type=str,
+                             help="The path of private key",
+                             required=True)
+    sign_parser.add_argument("-ts", "--to-sign-path", type=str,
+                             help="Path of object to sign",
+                             required=True)
+    sign_parser.add_argument("-o", "--output", type=str,
+                             help="Path to save the output",
+                             required=True)
 
-    sign_parser = subparsers.add_parser("sign_object")
-    sign_parser.add_argument("key_path", help="The path of private key")
-    sign_parser.add_argument("object_to_sign_path", help="Path of object to sign")
-    sign_parser.add_argument("output_path", help="Path to save the output")
+    verify_parser = sp.add_parser("verify",
+                                  help="Verify a signed object.")
+    verify_parser.add_argument("-f", "--object-to-verify", type=str,
+                               help="The path of signed object")
 
     args = parser.parse_args()
 
     if args.command == "generate_keypair":
-        serialized_keypair = generate_keypair(args.output_path, args.name)
+        serialized_keypair = generate_keypair(args.output, args.name)
         print(json.dumps(serialized_keypair, indent=4))
     # elif args.command == "prepare_public_cert":
     #     serialized_prepared_public_cert = prepare_public_cert(args.name)
     #     print(json.dumps(serialized_prepared_public_cert, indent=4))
-    elif args.command == "sign_object":
-        serialized_signed_payload_json = sign_object(args.key_path, args.object_to_sign_path, args.output_path)
+    elif args.command == "sign":
+        serialized_signed_payload_json = sign_object(args.key_path, args.to_sign_path, args.output)
         print(json.dumps(serialized_signed_payload_json, indent=4))
-
+    elif args.command == "verify":
+        if verify_object(args.object_to_verify):
+            print("Signature is valid.")
+        else:
+            print("Signature is invalid.")
